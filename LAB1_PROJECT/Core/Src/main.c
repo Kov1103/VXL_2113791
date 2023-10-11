54dec33c0704a32fd0ecfd9bfa2c87bc13667c13
hochianhkhoi2003 <khoi.hota1602@hcmut.edu.vn>
2023-10-11 22:31:23 +0700

    init for Ex10

--- LAB1_PROJECT/Core/Src/main.c
+++ LAB1_PROJECT/Core/Src/main.c
@@ -47,6 +47,10 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+void clearAllClock();
+void setNumberOnClock(int num);
+void clearNumberOnClock(int num);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -83,19 +87,126 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+  clearAllClock();
+  int second = 0, minute = 0, hour = 0;
+  int counter = 0;
   while (1)
   {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
+	  setNumberOnClock(second);
+	  setNumberOnClock(minute);
+	  setNumberOnClock(hour);
+	  HAL_Delay(1000);
+	  counter ++;
+	  if(counter >= 5){
+		  counter = 0;
+		  clearNumberOnClock(second++);
+	  }
+	  if(second >= 12){
+		  second = 0;
+		  clearNumberOnClock(minute++);
+	  }
+	  if(minute >= 12){
+		  minute = 0;
+		  clearNumberOnClock(hour++);
+	  }
+	  if(hour >= 12) hour = 0;
   }
-  /* USER CODE END 3 */
+}
+
+void clearAllClock(){
+	HAL_GPIO_WritePin (GPIOA , LED_1_Pin | LED_2_Pin | LED_3_Pin | LED_4_Pin | LED_5_Pin | LED_6_Pin | LED_7_Pin | LED_8_Pin | LED_9_Pin | LED_10_Pin | LED_11_Pin | LED_12_Pin , 1);
+}
+
+void setNumberOnClock(int num){
+	switch(num){
+	case 0:
+		HAL_GPIO_WritePin(GPIOA, LED_12_Pin, 0);
+		break;
+	case 1:
+		HAL_GPIO_WritePin(GPIOA, LED_1_Pin, 0);
+		break;
+	case 2:
+		HAL_GPIO_WritePin(GPIOA, LED_2_Pin, 0);
+		break;
+	case 3:
+		HAL_GPIO_WritePin(GPIOA, LED_3_Pin, 0);
+		break;
+	case 4:
+		HAL_GPIO_WritePin(GPIOA, LED_4_Pin, 0);
+		break;
+	case 5:
+		HAL_GPIO_WritePin(GPIOA, LED_5_Pin, 0);
+		break;
+	case 6:
+		HAL_GPIO_WritePin(GPIOA, LED_6_Pin, 0);
+		break;
+	case 7:
+		HAL_GPIO_WritePin(GPIOA, LED_7_Pin, 0);
+		break;
+	case 8:
+		HAL_GPIO_WritePin(GPIOA, LED_8_Pin, 0);
+		break;
+	case 9:
+		HAL_GPIO_WritePin(GPIOA, LED_9_Pin, 0);
+		break;
+	case 10:
+		HAL_GPIO_WritePin(GPIOA, LED_10_Pin, 0);
+		break;
+	case 11:
+		HAL_GPIO_WritePin(GPIOA, LED_11_Pin, 0);
+		break;
+	default:
+		break;
+	}
+}
+
+void clearNumberOnClock(int num){
+	switch(num){
+	case 0:
+		HAL_GPIO_WritePin(GPIOA, LED_12_Pin, 1);
+		break;
+	case 1:
+		HAL_GPIO_WritePin(GPIOA, LED_1_Pin, 1);
+		break;
+	case 2:
+		HAL_GPIO_WritePin(GPIOA, LED_2_Pin, 1);
+		break;
+	case 3:
+		HAL_GPIO_WritePin(GPIOA, LED_3_Pin, 1);
+		break;
+	case 4:
+		HAL_GPIO_WritePin(GPIOA, LED_4_Pin, 1);
+		break;
+	case 5:
+		HAL_GPIO_WritePin(GPIOA, LED_5_Pin, 1);
+		break;
+	case 6:
+		HAL_GPIO_WritePin(GPIOA, LED_6_Pin, 1);
+		break;
+	case 7:
+		HAL_GPIO_WritePin(GPIOA, LED_7_Pin, 1);
+		break;
+	case 8:
+		HAL_GPIO_WritePin(GPIOA, LED_8_Pin, 1);
+		break;
+	case 9:
+		HAL_GPIO_WritePin(GPIOA, LED_9_Pin, 1);
+		break;
+	case 10:
+		HAL_GPIO_WritePin(GPIOA, LED_10_Pin, 1);
+		break;
+	case 11:
+		HAL_GPIO_WritePin(GPIOA, LED_11_Pin, 1);
+		break;
+	default:
+		break;
+	}
 }
 
 /**
@@ -131,6 +242,36 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, LED_1_Pin|LED_2_Pin|LED_3_Pin|LED_4_Pin
+                          |LED_5_Pin|LED_6_Pin|LED_7_Pin|LED_8_Pin
+                          |LED_9_Pin|LED_10_Pin|LED_11_Pin|LED_12_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : LED_1_Pin LED_2_Pin LED_3_Pin LED_4_Pin
+                           LED_5_Pin LED_6_Pin LED_7_Pin LED_8_Pin
+                           LED_9_Pin LED_10_Pin LED_11_Pin LED_12_Pin */
+  GPIO_InitStruct.Pin = LED_1_Pin|LED_2_Pin|LED_3_Pin|LED_4_Pin
+                          |LED_5_Pin|LED_6_Pin|LED_7_Pin|LED_8_Pin
+                          |LED_9_Pin|LED_10_Pin|LED_11_Pin|LED_12_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
